7f0f3bb53ece915a50b26e576330a49fc918c76e
Keith Keller <kkeller@lbl.gov>
2017-05-02 12:12:04 -0700

    Added release notes Added check of current ACLs to add_read_acl (TASK-722)

--- lib/Bio/KBase/HandleMngr/HandleMngrImpl.pm
+++ lib/Bio/KBase/HandleMngr/HandleMngrImpl.pm
@@ -327,6 +327,27 @@ sub add_read_acl
 
 		my $header = HTTP::Headers->new('Authorization' => "OAuth " . $admin_token) ;
 
+                $ua->default_headers($header);
+
+                my $getResult = $ua->get($nodeurl."/acl?verbosity=full");
+                if (!$getResult->is_success) {
+			$succeeded{$handle->{hid}} = 0;
+			warn "Error: " . $getResult->message;
+			warn "Error: " . $getResult->content;
+                        next;                    
+                }
+                
+                my $jsonUsers=from_json($getResult->content);
+                my $users=$jsonUsers->{'data'}{'read'};
+                
+                if (grep { $_->{'username'} eq $username } @$users) {
+		    $succeeded{$handle->{hid}} = 1;
+                    warn "$username already has read access on $nodeurl, skipping PUT";
+                    next;
+                }
+
+                warn "setting read ACL on $nodeurl for $username";
+
 		my $req = new HTTP::Request("PUT",$nodeurl."/acl/read?users=$username",HTTP::Headers->new('Authorization' => "OAuth " . $admin_token));
 		$ua->prepare_request($req);
 		my $put = $ua->send_request($req);
